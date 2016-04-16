bd1d869cecea207d90220b5d15ffb41f54c62292
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-04-17 00:02:34 +0200

    testy dla nowych Utils2

--- test/Utils2Test.java
+++ test/Utils2Test.java
@@ -4,6 +4,11 @@ import utils.Math2;
 import utils.Matrix3;
 import utils.Utils2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Utils2Test {
 
     @Test
@@ -179,7 +184,7 @@ public class Utils2Test {
         double[][] dataset_n = {
                 {1, 2, 4, -4},
                 {2, 3, 1, 99},
-                {2.2, 4.1, 66},
+                {2.2, 4.1, 66, 1},
                 {0, 0, 0, 2}
         };
         double[][] mean_n = Utils2.random_mean_n(dataset_n);
@@ -191,5 +196,118 @@ public class Utils2Test {
 
             Assert.assertTrue(isOk);
         }
+    }
+
+    @Test
+    public void random_mean_n2_test_true2() {
+        double[][] dataset_n = {
+                {1, 2, 4, -4},
+                {2, 3, 1, 99},
+                {2.2, 4.1, 66, 1},
+                {0, 0, 0, 2}
+        };
+        double[][] mean_n = Utils2.random_mean_n2(dataset_n);
+
+        boolean isOk;
+        for (int i = 0; i < dataset_n.length; i++) {
+            isOk = mean_n[i][0] >= Math2.min(dataset_n[i]);
+            isOk &= mean_n[i][0] <= Math2.max(dataset_n[i]);
+
+            Assert.assertTrue(isOk);
+        }
+    }
+
+    @Test
+    public void to_int_array_test_true1() {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(5);
+        integers.add(0);
+        int[] expected = {1, 2, 5, 0};
+
+        int[] result = Utils2.to_int_array(integers);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void to_int_array_test_true2() {
+        List<Integer> integers = new ArrayList<>();
+        int[] expected = {};
+
+        int[] result = Utils2.to_int_array(integers);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void to_dbl_array_test_true1() {
+        List<Double> dbls = new ArrayList<>();
+        dbls.add(1.0);
+        dbls.add(2.0);
+        dbls.add(5.1);
+        dbls.add(0.0);
+        double[] expected = {1, 2, 5.1, 0};
+
+        double[] result = Utils2.to_dbl_array(dbls);
+        Assert.assertArrayEquals(expected, result, 1e-10);
+    }
+
+    @Test
+    public void to_dbl_array_test_true2() {
+        List<Double> dbls = new ArrayList<>();
+        double[] expected = {};
+
+        double[] result = Utils2.to_dbl_array(dbls);
+        Assert.assertArrayEquals(expected, result, 1e-10);
+    }
+
+    @Test
+    public void inflection_point_test_true1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void empty_lists_ids_test_true1() {
+        List<List<String>> data = new ArrayList<>();
+        data.add(new ArrayList<>());
+        data.add(Arrays.asList("1", "2", "3"));
+        data.add(Collections.singletonList("2"));
+        data.add(new ArrayList<>());
+        data.add(Arrays.asList("1", "2"));
+
+        int[] expected = {3, 0};
+        int[] result = Utils2.empty_lists_ids(data);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void empty_lists_ids_test_true2() {
+        List<List<String>> data = new ArrayList<>();
+        data.add(new ArrayList<>());
+        data.add(new ArrayList<>());
+
+        int[] expected = {1, 0};
+        int[] result = Utils2.empty_lists_ids(data);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void empty_lists_ids_test_true3() {
+        List<List<String>> data = new ArrayList<>();
+        data.add(Arrays.asList("1", "2", "3"));
+
+        int[] expected = {};
+        int[] result = Utils2.empty_lists_ids(data);
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void empty_lists_ids_test_true4() {
+        List<List<String>> data = new ArrayList<>();
+        data.add(Arrays.asList(null, null));
+
+        int[] expected = {};
+        int[] result = Utils2.empty_lists_ids(data);
+        Assert.assertArrayEquals(expected, result);
     }
 }
